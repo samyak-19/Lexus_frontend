605762d6a953bc75c8e7c5440bcf7efba9556f9d
samyak-19 <samlinkin19@gmail.com>
2026-07-04 14:37:46 +0530

    Initial frontend commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://lexus-backend.onrender.com",
+  baseURL: process.env.NEXT_PUBLIC_API_URL ||
+    "http://localhost:5000/api",
 });
 
 api.interceptors.request.use((config) => {
